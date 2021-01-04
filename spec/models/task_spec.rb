a5dc4148781d0cd551f2c649d5a9349139882e02
信定隆志 <nobusadatakashi@mba.local>
2021-01-04 22:46:36 +0900

    fix: データ作成時のFactoryBot呼び出しの省略

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'is valid with all attributes' do
-      expect(FactoryBot.build(:task)).to be_valid
+      expect(build(:task)).to be_valid
     end
 
     it 'is invalid without title' do
-      task = FactoryBot.build(:task, title: nil)
+      task = build(:task, title: nil)
       task.valid?
       expect(task.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without status' do
-      task = FactoryBot.build(:task, status: nil)
+      task = build(:task, status: nil)
       task.valid?
       expect(task.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
-      FactoryBot.create(:task, title: 'title')
-      task = FactoryBot.build(:task, title: 'title')
+      create(:task, title: 'title')
+      task = build(:task, title: 'title')
       task.valid?
       expect(task.errors[:title]).to include("has already been taken")
     end
 
     it 'is valid with another title' do
-      FactoryBot.create(:task, title: 'title1')
-      expect(FactoryBot.build(:task, title: 'title2')).to be_valid
+      create(:task, title: 'title1')
+      expect(build(:task, title: 'title2')).to be_valid
     end
   end
 end
