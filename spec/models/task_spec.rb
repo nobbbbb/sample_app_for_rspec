63fb1ed2726991f43ee29eb7c608602c6360f580
信定隆志 <nobusadatakashi@mba.local>
2021-01-04 23:33:22 +0900

    fix: 変数名を役割がわかるように変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,27 +7,28 @@ RSpec.describe Task, type: :model do
     end
 
     it 'is invalid without title' do
-      task = build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: nil)
+      task_without_title.valid?
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without status' do
-      task = build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      task_without_status.valid?
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
       create(:task, title: 'title')
-      task = build(:task, title: 'title')
-      task.valid?
-      expect(task.errors[:title]).to include("has already been taken")
+      task_with_same_title = build(:task, title: 'title')
+      task_with_same_title.valid?
+      expect(task_with_same_title.errors[:title]).to include("has already been taken")
     end
 
     it 'is valid with another title' do
       create(:task, title: 'title1')
-      expect(build(:task, title: 'title2')).to be_valid
+      task_with_another_title = build(:task, title: 'title2')
+      expect(task_with_another_title).to be_valid
     end
   end
 end
