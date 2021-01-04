4f6389a6cd14f6683fc5d6a99e7c79451ca4b63a
信定隆志 <nobusadatakashi@mba.local>
2021-01-04 23:42:28 +0900

    fix: taskのtitleをString#nextを利用する形に変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :task do
-    sequence(:title) { |n| "title#{n}"}
+    sequence(:title, "title_1")
+    content { "content"}
     status { :todo }
+    deadline { 1.week.from_now }
     association :user
   end
 end
